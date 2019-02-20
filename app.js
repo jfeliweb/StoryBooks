0cd6cd0dbc84fdd0b192bf331fb21f3876e06ea1
jfelisme <jfelisme@fiu.edu>
2019-02-19 21:05:30 -0500

    add UserSchema

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 const express = require('express'),
-      mongoose = require('mongoose'),
-      passport = require('passport');
+    mongoose = require('mongoose'),
+    passport = require('passport');
 
 // Passport Config
 require('./config/passport')(passport);
 
 // Load Routes
 const auth = require('./routes/auth');
+
+// Load Keys
+const keys = require('./config/keys');
+
+// Map global promises
+mongoose.Promise = global.Promise;
+// Mongoose Connect
+mongoose.connect(keys.mongoURI, {
+        useMongoClient: true
+    })
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
 
 const app = express();
 
