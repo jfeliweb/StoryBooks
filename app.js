a06e39b9c9d878c5de37022d7c9d23908ba4d033
jfelisme <jfelisme@fiu.edu>
2019-02-22 15:53:01 -0500

    add stripTags and truncate to stories template

--- app.js
+++ app.js
@@ -22,6 +22,11 @@ const stories = require('./routes/stories');
 // Load Keys
 const keys = require('./config/keys');
 
+// Handlebars Helpers
+const {
+    truncate,
+    stripTags
+} = require('./helpers/hbs');
 // Map global promises
 mongoose.Promise = global.Promise;
 // Mongoose Connect
@@ -41,6 +46,10 @@ app.use(bodyParser.json());
 
 // Handlebars Middleware
 app.engine('handlebars', exphbs({
+    helpers: {
+        truncate: truncate,
+        stripTags: stripTags
+    },
     defaultLayout: 'main'
 }));
 app.set('view engine', 'handlebars');
